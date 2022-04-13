c42a649cfa914e548f70314dc51ecd03bbfd6df5
Dennis Bär <dennis.baer@student.reutlingen-university.de>
2022-04-13 16:13:04 +0200

    Main Page UI Simple Prototype

--- app/src/main/java/com/dennisdevelops/simple_vitamin_task_reminder/MainActivity.kt
+++ app/src/main/java/com/dennisdevelops/simple_vitamin_task_reminder/MainActivity.kt
@@ -3,18 +3,21 @@ package com.dennisdevelops.simple_vitamin_task_reminder
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.animation.animateColorAsState
 import androidx.compose.foundation.layout.*
 import androidx.compose.foundation.lazy.LazyColumn
-import androidx.compose.foundation.lazy.items
+import androidx.compose.foundation.lazy.itemsIndexed
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material.*
 import androidx.compose.runtime.*
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextDecoration
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
-
 import com.dennisdevelops.simple_vitamin_task_reminder.ui.theme.SimplevitamintaskreminderTheme
 
 class MainActivity : ComponentActivity() {
@@ -48,11 +51,22 @@ class MainActivity : ComponentActivity() {
                             mutableStateListOf("Multivitamin", "Omega-3", "Sport")
                         }
 
-                        Column(modifier = Modifier.padding(16.dp)) {
+                        Column(modifier = Modifier.padding(horizontal = 16.dp)) {
                             Heading("Today")
-                            LazyColumn {
-                                items(taskList) { task ->
-                                    TaskCard(name = task)
+                            Spacer(modifier = Modifier.height(20.dp))
+                            LazyColumn(modifier = Modifier.fillMaxSize()) {
+                                itemsIndexed(taskList) { index, task ->
+                                    TaskCard(title = task, description = "Lorem ipsum dolor")
+
+                                    if (index != task.length - 1) {
+                                        Spacer(modifier = Modifier.height(10.dp))
+                                    }
+                                }
+
+                                items(10) {
+                                    TaskCard(title = "Test", description = "Lorem ipsum dolor")
+                                    Spacer(modifier = Modifier.height(10.dp))
+
                                 }
                             }
                         }
@@ -73,13 +87,15 @@ fun Heading(name: String) {
 
 @Composable
 
-fun TaskCard(name: String) {
+fun TaskCard(title: String, description: String? = null) {
 
     var isChecked by remember { mutableStateOf(false) }
     Card(
-        elevation = 12.dp, modifier = Modifier
+        elevation = 12.dp,
+        modifier = Modifier
             .fillMaxWidth()
-            .height(100.dp)
+            .height(100.dp),
+        shape = RoundedCornerShape(10.dp)
     ) {
 
         Column(
@@ -93,10 +109,17 @@ fun TaskCard(name: String) {
                 verticalAlignment = Alignment.CenterVertically,
                 horizontalArrangement = Arrangement.SpaceBetween
             ) {
-                Text(text = name)
+
+                TextForTextCard(
+                    title = title,
+                    description = description,
+                    modifier = Modifier,
+                    completed = isChecked
+                )
                 Checkbox(checked = isChecked, onCheckedChange = {
                     isChecked = it
                 })
+
             }
         }
 
@@ -104,10 +127,54 @@ fun TaskCard(name: String) {
     }
 }
 
+@Composable
+fun TextForTextCard(
+    title: String,
+    description: String? = null,
+    modifier: Modifier,
+    completed: Boolean
+) {
+
+    val textColor by animateColorAsState(targetValue = if (completed) Color.Gray else Color.Black)
+    Column(
+        modifier = modifier,
+        verticalArrangement = Arrangement.Center,
+        horizontalAlignment = Alignment.Start
+    ) {
+        Text(
+            text = title,
+            fontSize = 16.sp,
+            color = textColor,
+            textDecoration = if (completed) TextDecoration.LineThrough else TextDecoration.None
+        )
+        if (description != null) {
+            Text(
+                text = description,
+                fontWeight = FontWeight.Light,
+                fontSize = 10.sp,
+                color = textColor,
+                textDecoration = if (completed) TextDecoration.LineThrough else TextDecoration.None
+            )
+
+        }
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun PreviewText() {
+    SimplevitamintaskreminderTheme {
+        TaskCard(title = "Multivitamin", description = "Take the Vitamins every day only once")
+
+
+    }
+}
+
+
 @Preview(showBackground = true)
 @Composable
 fun DefaultPreview() {
     SimplevitamintaskreminderTheme {
-        TaskCard(name = "Multivitamin")
+        TaskCard(title = "Multivitamin")
     }
 }
